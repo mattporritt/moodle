ee463277e1c1a8c751441e66d46d388070fa93b4
Matt Porritt <matt.porritt@moodle.com>
2024-06-18 15:37:08 +1000

    fix namespace regression

--- ai/provider/azureai/classes/privacy/provider.php
+++ ai/provider/azureai/classes/privacy/provider.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace aiprovider_azureai\privacy;
 
 use core_privacy\local\metadata\null_provider;
 
