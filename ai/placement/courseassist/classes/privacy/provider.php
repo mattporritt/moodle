aaef85493ae2c6173708d3f08885c7b8bd791883
Matt Porritt <matt.porritt@moodle.com>
2024-06-18 14:46:07 +1000

    fix privacy provider namespaces

--- ai/placement/courseassist/classes/privacy/provider.php
+++ ai/placement/courseassist/classes/privacy/provider.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace aiplacement_courseassist\privacy;
 
 use core_privacy\local\metadata\null_provider;
 
