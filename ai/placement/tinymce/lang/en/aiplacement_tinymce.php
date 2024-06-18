5a2e5cf12ceea3c2012a1ef7521ea4ae4eab1db0
Matt Porritt <matt.porritt@moodle.com>
2024-06-18 14:56:47 +1000

    add initial tunymce placement boilerplate

---
+++ ai/placement/tinymce/lang/en/aiplacement_tinymce.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component aiplacement_tinymce, language 'en'.
+ *
+ * @package    aiplacement_tinymce
+ * @copyright  2024 Matt Porritt <matt.porritt@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'TinyMCE Editor Placement';
+$string['generateimagesetting'] = 'Generate Images';
+$string['generateimagesetting_desc'] = 'Enable or disable the generation of images from a text prompt.';
+$string['generatetextsetting'] = 'Generate Text';
+$string['generatetextsetting_desc'] = 'Enable or disable the generation of text from a text prompt.';
+
+$string['privacy:metadata'] = 'The TinyMCE editor placement plugin does not store any personal data.';
+
+
