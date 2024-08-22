03b1d9a90d9d36c0ed08bff606c005045418623e
Huong Nguyen <huongnv13@gmail.com>
2024-08-22 15:58:10 +0700

    MDL-82627 AI: Code cleanup
    Co-authored-by: Meirza <meirza.arson@moodle.com>

--- lib/db/services.php
+++ lib/db/services.php
@@ -3239,7 +3239,6 @@ $functions = array(
     ],
     'core_ai_set_policy_status' => [
         'classname'   => 'core_ai\external\set_policy_status',
-        'methodname'  => 'set_policy_status',
         'description' => 'Set a users AI policy acceptance',
         'type'        => 'write',
         'ajax'        => true,
@@ -3247,7 +3246,6 @@ $functions = array(
     ],
     'core_ai_get_policy_status' => [
         'classname'   => 'core_ai\external\get_policy_status',
-        'methodname'  => 'get_policy_status',
         'description' => 'Get a users AI policy acceptance',
         'type'        => 'read',
         'ajax'        => true,
